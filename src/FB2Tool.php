1078f3880ab72d9798e2076bffd73a65d46c8b1d
Inkoder <web.inkoder@gmail.com>
2022-05-05 00:19:50 +0300

    Start of parse method @inProgress

--- src/FB2Tool.php
+++ src/FB2Tool.php
@@ -9,15 +9,52 @@
 namespace Inkoder\Fb2tool;
 
 use Inkoder\Fb2tool\Helpers\XmlHelper;
+use Inkoder\Fb2tool\Models\Book;
 
 class FB2Tool
 {
+    /**
+     * @var
+     */
+    private $bookXml;
+    private $book;
 
     public function __construct($file = '')
     {
-        return XmlHelper::load($file);
+        try {
+            $this->setBookXml(XmlHelper::load($file));
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
+    /**
+     * @return mixed
+     */
+    public function getBookXml()
+    {
+        return $this->bookXml;
+    }
+
+    /**
+     * @param mixed $bookXml
+     *
+     * @throws \Exception
+     */
+    public function setBookXml($bookXml): void
+    {
+        if ($bookXml instanceof \SimpleXMLElement) {
+            $this->bookXml = $bookXml;
+        } else {
+            throw new \Exception('Error getting book xml');
+        }
+    }
+
+    public function parse() {
+
+        $this->book = new Book();
+
+    }
 
 
 }
