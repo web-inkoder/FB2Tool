8293467e3fffb30e6f2744120c37e0c93beeb212
Inkoder <web.inkoder@gmail.com>
2022-05-09 22:20:52 +0300

    Add CustomInfo model

---
+++ src/Models/CustomInfo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * For: FB2Tool
+ * User: Sergey Akulov (web.inkoder@gmail.com)
+ * Date: 09.05.2022
+ */
+
+namespace Inkoder\Fb2tool\Models;
+
+class CustomInfo implements IModel
+{
+    private $infoType;
+}
